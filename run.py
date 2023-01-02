4b3d9b435b9da6ce45a562ffc8e66c2bffeb1ff5
mjrosi <mrostamkhani@gmail.com>
2023-01-02 22:46:38 +0000

    create a while loop inside the function add_item

--- run.py
+++ run.py
@@ -3,8 +3,22 @@ shopping_list = []
 # Adds an item to the shopping list
 
 def add_item():
-    item = input("Enter the item you wish to add to the shopping list: ")
-    shopping_list.append(item)
-    print(item + " has been added to the shopping list.")
+    stop = False
+    while not stop:
+        item = input("Enter the item you wish to add to the shopping list: ")
+        shopping_list.append(item)
+        print(item + " has been added to the shopping list.")
+        user_input = input("Would you like to enter another item?\nType 'c' for continue or 'q' to quit:\n")
+        if user_input == 'q':
+            stop = True
     
 add_item()
+
+# Displays all items on the shopping list
+def display_list():
+    print()
+    print("--- SHOPPING LIST ---")
+    for i in shopping_list:
+        print("* " + i)
+
+display_list()
