c3f5cda0d47f47cf809a737d187ab1b1bb291ac1
mjrosi <mrostamkhani@gmail.com>
2023-01-03 01:03:21 +0000

    add docstring to function

--- run.py
+++ run.py
@@ -3,24 +3,35 @@ import sys
 
 shopping_list = []
 
+
 # Adds an item to the shopping list
 def add_item():
+    """
+    Get item input from the user to add to the shopping list.
+    Run a while loop to collect a valid input from the user
+    via the terminal, which must be a string.
+    The loop will repeatedly request input item, until it stop by user
+    by inserting q.
+    """
     stop = False
     while not stop:
         item = input("Enter the item you wish to add to the shopping list: ")
-        shopping_list.append(item)
+        shopping_list.append(item.capitalize())
         print(f"{item} has been added to the shopping list.")
         user_input = input("Would you like to add another item?\nType 'c' to continue or 'q' to quit:\n")
         if user_input == 'q':
             stop = True
+            display_list()
 
 
 # Displays all items on the shopping list
 def display_list():
-    print()
-    print("--- SHOPPING LIST ---")
-    for i in shopping_list:
-        print("* " + i)
+    if len(shopping_list) == 0:
+        print("The shopping list is empty")
+    else:
+        print("--- SHOPPING LIST ---")
+        for i in shopping_list:
+            print("* " + i)
 
 
 # Remove an item from the shopping list
@@ -45,7 +56,8 @@ def check_item():
             print(f"{item} has not been added to the shopping list.")
     display_list()
 
-# How many items are on the shopping list        
+
+# How many items are on the shopping list
 def shopping_list_length():
     number_of_items = len(shopping_list)
     print(f"There are {number_of_items} items on the shopping list.")
@@ -58,9 +70,7 @@ def clear_shopping_list():
 
 
 def main():
-    while True:
-        print()
-        print('''### SHOPPING LIST ###
+    print('''### SHOPPING LIST ###
 
         Select a number for the action that you would like to do:
 
@@ -73,27 +83,27 @@ def main():
         7. Exit
         ''')
 
-        # Ask the user to make a selection
-        selection = input("Make your selection: ") 
+    # Ask the user to make a selection
+    selection = input("Make your selection: ")
 
-        # Determine which action to perform based on the user's selection
-        if selection == "1":
-            add_item()            
-        elif selection == "2":
-            display_list()
-        elif selection == "3":
-            remove_item()
-        elif selection == "4":
-            check_item()
-        elif selection == "5":
-            shopping_list_length()
-        elif selection == "6":
-            clear_shopping_list()
-        elif selection == "7":
-            sys.exit()
-        else:
-            print("You did not make a valid selection.")
+    # Determine which action to perform based on the user's selection
+    if selection == "1":
+        add_item()
+    elif selection == "2":
+        display_list()
+    elif selection == "3":
+        remove_item()
+    elif selection == "4":
+        check_item()
+    elif selection == "5":
+        shopping_list_length()
+    elif selection == "6":
+        clear_shopping_list()
+    elif selection == "7":
+        sys.exit()
+    else:
+        print("You did not make a valid selection.")
 
 
-# Run the function mainMenu - which will run our app    
+# Run the function mainMenu - which will run our app
 main()
